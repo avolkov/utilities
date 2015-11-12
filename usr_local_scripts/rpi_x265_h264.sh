728745f635e9219f382a76f9220d7a8fdc5a2d04
Alex Volkov <alex@flamy.ca>
2015-11-12 09:35:50 -0500

    Re-encode x265 videos into h264

--- usr_local_scripts/rpi_x265_h264.sh
+++ usr_local_scripts/rpi_x265_h264.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+## http://superuser.com/questions/785528/how-to-generate-an-mp4-with-h-265-codec-using-ffmpeg
 IFS='
 '
-mkdir -f out
+mkdir -p out
 
-for k in $(find . -maxdepth 0 -type f)
+for k in *.mkv
 do
-    /usr/bin/ffmpeg -i ${k} -an -x265-params crf=25  out/${k}
+    /usr/bin/ffmpeg -i ${k} -c:a copy -x265-params crf=25 out/${k}
 done
